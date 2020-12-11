d11677ec1bdf23d26c2c180054673a3bd15ea87b
Harry <nkjang@gmail.com>
2020-12-11 21:52:27 +0900

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+# Redis
+### 실행
+```
+docker-compose up -d
+```
+### port
+```
+localhost:6379 -> docker:6379
+```
---
+++ src/main/java/my/SpringRedisApplication.java
@@ -0,0 +1,11 @@
+package my;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRedisApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRedisApplication.class, args);
+    }
+}
